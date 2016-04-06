ffae71621a534dfe74200542ed737d3c7aeb6cb4
VergeA <andrew.verge64@gmail.com>
2016-04-06 08:54:42 -0400

    Initial html generation

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run()
 
+
+#host="0.0.0.0"
